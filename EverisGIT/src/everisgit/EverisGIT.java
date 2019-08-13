20d870c26cd73e87f8b01fe386b400ff37f0a952
Fabrizio Pouget <f.pouget94@gmail.com>
2019-08-13 11:09:49 -0300

    Segundo Commit Operaciones

--- EverisGIT/src/everisgit/EverisGIT.java
+++ EverisGIT/src/everisgit/EverisGIT.java
@@ -16,6 +16,16 @@ public class EverisGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a=4,b=2;
+        Suma obj1= new Suma();
+        Resta obj2= new Resta();
+        Multiplicacion obj3= new Multiplicacion();
+        Division obj4 = new Division();
+        
+        System.out.println(obj1.sumar(a, b));
+        System.out.println(obj2.restar(a, b));
+        System.out.println(obj3.multiplicar(a, b));
+        System.out.println(obj4.dividir(a, b));
     }
     
 }
